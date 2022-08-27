6c91678e0e1cbf645bdac9404cbc34eaa884e48e
pujingkang <58412001+pujingkang@users.noreply.github.com>
2022-08-27 15:04:53 +0800

    pull commit!

--- src/main/java/com/souls/git/GitTest.java
+++ src/main/java/com/souls/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix commit!");
         System.out.println("push commit!");
         System.out.println("push commit!");
+        System.out.println("pull commit!");
     }
 }
